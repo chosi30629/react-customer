3d75b0669130d36251751590af7d067f7861854a
조성일 <chosi30629@gmail.com>
2020-02-02 21:26:41 +0900

    React의 라이프 사이클 이해 및 API 로딩 처리 구현하기

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import TableHead from "@material-ui/core/TableHead";
 import TableBody from "@material-ui/core/TableBody";
 import TableRow from "@material-ui/core/TableRow";
 import TableCell from "@material-ui/core/TableCell";
+import CircularProgress from "@material-ui/core/CircularProgress";
 import { withStyles } from "@material-ui/core/styles";
 
 const styles = theme => ({
@@ -17,15 +18,20 @@ const styles = theme => ({
   },
   table: {
     minWidth: 1080
+  },
+  progress: {
+    margin: theme.spacing(2)
   }
 });
 
 class App extends React.Component {
   state = {
-    customers: ""
+    customers: "",
+    completed: 0
   };
 
   componentDidMount() {
+    this.timer = setInterval(this.progress, 20);
     this.callApi()
       .then(res => this.setState({ customers: res }))
       .catch(err => console.log(err));
@@ -35,6 +41,11 @@ class App extends React.Component {
     const response = await fetch("/api/customers");
     const body = await response.json();
     return body;
+  };
+
+  progress = () => {
+    const { completed } = this.state;
+    this.setState({ completed: completed >= 100 ? 0 : completed + 1 });
   };
 
   render() {
@@ -68,7 +79,15 @@ class App extends React.Component {
                 );
               })
             ) : (
-              <TableRow></TableRow>
+              <TableRow>
+                <TableCell colSpan="6" align="center">
+                  <CircularProgress
+                    className={classes.progress}
+                    variant="determinate"
+                    value={this.state.completed}
+                  />
+                </TableCell>
+              </TableRow>
             )}
           </TableBody>
         </Table>

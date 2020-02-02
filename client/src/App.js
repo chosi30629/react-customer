64d7330100fa6fb354e3c58554e07cb936facad3
조성일 <chosi30629@gmail.com>
2020-02-02 21:18:14 +0900

    Node.js Express에서 REST API 구축하기

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ import { withStyles } from "@material-ui/core/styles";
 const styles = theme => ({
   root: {
     width: "100%",
-    marginTop: theme.spacing.unit * 3,
+    marginTop: theme.spacing(3),
     overflowX: "auto"
   },
   table: {
@@ -20,66 +20,61 @@ const styles = theme => ({
   }
 });
 
-const customers = [
-  {
-    id: 1,
-    image: "https://placeimg.com/64/64/1",
-    name: "조성일",
-    birthday: "920920",
-    gender: "남자",
-    job: "개발자"
-  },
-  {
-    id: 2,
-    image: "https://placeimg.com/64/64/2",
-    name: "홍길동",
-    birthday: "900710",
-    gender: "남자",
-    job: "디자이너"
-  },
-  {
-    id: 3,
-    image: "https://placeimg.com/64/64/3",
-    name: "김하하",
-    birthday: "910310",
-    gender: "여자",
-    job: "사장"
-  }
-];
+class App extends React.Component {
+  state = {
+    customers: ""
+  };
 
-function App(props) {
-  const { classes } = props;
-  return (
-    <Paper className={classes.root}>
-      <Table className={classes.table}>
-        <TableHead>
-          <TableRow>
-            <TableCell>번호</TableCell>
-            <TableCell>이미지</TableCell>
-            <TableCell>이름</TableCell>
-            <TableCell>생년월일</TableCell>
-            <TableCell>성별</TableCell>
-            <TableCell>직업</TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {customers.map(c => {
-            return (
-              <Customer
-                key={c.id}
-                id={c.id}
-                image={c.image}
-                name={c.name}
-                birthday={c.birthday}
-                gender={c.gender}
-                job={c.job}
-              />
-            );
-          })}
-        </TableBody>
-      </Table>
-    </Paper>
-  );
+  componentDidMount() {
+    this.callApi()
+      .then(res => this.setState({ customers: res }))
+      .catch(err => console.log(err));
+  }
+
+  callApi = async () => {
+    const response = await fetch("/api/customers");
+    const body = await response.json();
+    return body;
+  };
+
+  render() {
+    const { classes } = this.props;
+    return (
+      <Paper className={classes.root}>
+        <Table className={classes.table}>
+          <TableHead>
+            <TableRow>
+              <TableCell>번호</TableCell>
+              <TableCell>이미지</TableCell>
+              <TableCell>이름</TableCell>
+              <TableCell>생년월일</TableCell>
+              <TableCell>성별</TableCell>
+              <TableCell>직업</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody>
+            {this.state.customers ? (
+              this.state.customers.map(c => {
+                return (
+                  <Customer
+                    key={c.id}
+                    id={c.id}
+                    image={c.image}
+                    name={c.name}
+                    birthday={c.birthday}
+                    gender={c.gender}
+                    job={c.job}
+                  />
+                );
+              })
+            ) : (
+              <TableRow></TableRow>
+            )}
+          </TableBody>
+        </Table>
+      </Paper>
+    );
+  }
 }
 
 export default withStyles(styles)(App);
--- server.js
+++ server.js
@@ -6,8 +6,33 @@ const port = process.env.PORT || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get("/api/hello", (req, res) => {
-  res.send({ message: "hello Express!" });
+app.get("/api/customers", (req, res) => {
+  res.send([
+    {
+      id: 1,
+      image: "https://placeimg.com/64/64/1",
+      name: "조성일",
+      birthday: "920920",
+      gender: "남자",
+      job: "개발자"
+    },
+    {
+      id: 2,
+      image: "https://placeimg.com/64/64/2",
+      name: "홍길동",
+      birthday: "900710",
+      gender: "남자",
+      job: "디자이너"
+    },
+    {
+      id: 3,
+      image: "https://placeimg.com/64/64/3",
+      name: "김하하",
+      birthday: "910310",
+      gender: "여자",
+      job: "사장"
+    }
+  ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));

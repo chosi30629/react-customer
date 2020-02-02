0781df6c4fe5ff77ca3427da49e928b41cf5d311
조성일 <chosi30629@gmail.com>
2020-02-02 20:03:18 +0900

    고객 컴포넌트 만들기

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from "react";
-import logo from "./logo.svg";
 import "./App.css";
+import Customer from "./components/Customer";
+
+const customer = {
+  name: "조성일",
+  birthday: "920920",
+  gender: "남자",
+  job: "개발자"
+};
 
 function App() {
   return (
-    <div className="gray-background">
-      <img src={logo} lat="logo" />
-      <h2>Let's develop management system!</h2>
-    </div>
+    <Customer
+      name={customer.name}
+      birthday={customer.birthday}
+      gender={customer.gender}
+      job={customer.job}
+    />
   );
 }
 
